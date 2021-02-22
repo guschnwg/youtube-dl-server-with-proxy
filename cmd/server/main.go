9dc4ea939c4992997a5ddc4ee3054cc05c50cf37
Gustavo Alpendere <gustavo.alpendere@nextroll.com>
2021-02-21 23:54:31 -0300

    test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -11,9 +13,16 @@ import (
 func main() {
 	port := os.Getenv("PORT")
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"results": "OI",
+		})
+	})
 	http.HandleFunc("/api/info", server.YoutubeInfo)
 
 	server.BindProxy()
+
+	fmt.Printf("Listening in port %s", port)
 
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
